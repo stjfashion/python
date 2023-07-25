a769b39c6ea6957b31606f04eee20c41f7483a15
Stella <139699736+stjfashion@users.noreply.github.com>
2023-07-25 23:05:37 +0400

    Update rectangle.py

--- rectangle.py
+++ rectangle.py
@@ -1,6 +1,6 @@
 l = float(input("Enter the length of the rectangle: "))
 w = float(input("Enter the width of the rectangle: "))
 
-area = length * width
+area = l * w
 
 print("The area of the rectangle is:", area)
