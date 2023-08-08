b7ccf73e3fbd598a3dff926ba647d3f44b55dabc
Stella <139699736+stjfashion@users.noreply.github.com>
2023-08-09 00:15:28 +0400

    Update divide.py

--- divide.py
+++ divide.py
@@ -1,6 +1,6 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-quot = num1 / num2
+quot = num1 / num2  # num1 & num2 variables from user
 
 print("The quotient of", num1, "and", num2, "is", quot)
