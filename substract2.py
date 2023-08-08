a892132ab4402e552fd6484702350bac1ab11627
Stella <139699736+stjfashion@users.noreply.github.com>
2023-08-08 22:51:53 +0400

    Update substract2.py

--- substract2.py
+++ substract2.py
@@ -1,6 +1,6 @@
 num1 = float(input("Enter the first number: "))
 num2 = float(input("Enter the second number: "))
 
-diff = num1 - num2
+diff = num1 - num2  # substract 2 vars
 
 print("The difference between", num1, "and", num2, "is", diff)
